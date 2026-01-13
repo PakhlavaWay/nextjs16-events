ea33915eb099dc0e54f077bc384d32f8336654e7
PakhlavaWay <zhorobaev0707@gmail.com>
2026-01-13 11:20:52 -0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
+
+
+
+const Page = () => {
+  return (
+    <section>
+      <h1 className="text-center">The hub for every Dev <br /> event you cant miss</h1>
+      <p className="text-center mt-5">Hackatons, meetup, and Conferences, All in one Place</p>
+
+      <ExploreBtn />
+
+     <div className="mt-20 space-y-7">
+      <h3>Featured Events</h3>
+
+      <ul className="events">
+        {events.map((event) => (
+          <li key={event.title}>
+            <EventCard {...event} />
+          </li>
+          
+        ))}
+        
+      </ul>
+     </div>
+    </section>
+  );
+};
+
+export default Page;
